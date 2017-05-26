5422f86b3c05c3baecfc8261877c9de61df82ade
Sebastian Ferrari <sebas@taller.net.br>
2017-05-26 01:45:09 -0300

    Added get book by ISBN to client.

--- src/GoodReadsClientService.php
+++ src/GoodReadsClientService.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Drupal\goodreads;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
 
 /**
  * Class GoodReadsClientService.
@@ -9,10 +11,31 @@ namespace Drupal\goodreads;
  */
 class GoodReadsClientService implements GoodReadsClientServiceInterface {
 
+  const key = 'pc79i9QZUmG5Eb7CWvnsGA';
+  const urlBase = 'https://www.goodreads.com/';
+
+  private $client;
+
   /**
    * Constructs a new GoodReadsClientService object.
    */
-  public function __construct() {
-
+  public function __construct(Client $client) {
+    $this->client = $client;
   }
+
+  public function getBookByISBN($id) {
+    $response = $this->client->get(self::urlBase . 'book/isbn/' . $id, [
+      'query' => [
+        'key' => self::key,
+        'format' => 'xml'
+      ],
+    ]);
+
+    return $this->parseResponse($response);
+  }
+
+  public function parseResponse(Response $response) {
+    return new \SimpleXMLElement($response->getBody()->getContents());
+  }
+
 }
